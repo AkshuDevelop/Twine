2b83c164aa3831dec7f5fefe397460087b8e6fe1
Akshu <akshatkumarjha2005@gmail.com>
2025-04-02 11:43:25 +0530

    Initial commit: Twine

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div className="min-h-screen w-full bg-black">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
